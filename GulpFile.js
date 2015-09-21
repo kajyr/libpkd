bad9c0c28506fa61e3ee5d27fb3f4753e0cae38a
kajyr <carlo.panzi@gmail.com>
2015-09-21 17:46:00 +0200

    build semplice

--- GulpFile.js
+++ GulpFile.js
@@ -1,7 +1,6 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var coffee = require('gulp-coffee');
-var concat = require('gulp-concat');
 
 var lib = {
 	source: './src/**/*.coffee',
@@ -10,7 +9,6 @@ var lib = {
 
 gulp.task('build', function() {
 	gulp.src(lib.source)
-		.pipe(concat('index.js'))
 		.pipe(coffee({bare: false}).on('error', gutil.log))
 		.pipe(gulp.dest('./'))
 });
